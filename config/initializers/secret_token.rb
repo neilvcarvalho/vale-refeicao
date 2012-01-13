a6e20339f8b249d81825f2806268d3ffbe34cb5a
Neilson Carvalho <neilson@maino.com.br>
2012-01-13 11:43:06 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VR::Application.config.secret_token = 'a300544951e737545740f111431318975ef1631d77db35b56d3e9d0e6b2c0289235cd8786baba5806c622b1616a3df897ef81b945c4e7db33bc182f6dff3ddb2'
